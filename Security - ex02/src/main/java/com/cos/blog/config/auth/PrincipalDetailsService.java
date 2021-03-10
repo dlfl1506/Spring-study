20a1e8f060d92bfae05ea2da01268c45ff01885d
YD <hoggaz@naver.com>
2021-03-10 13:27:50 +0900

    5-1 스프링부트 시큐리티 실습 예제 2 (시큐리티 커스터마이징,회원가입,로그인,인증)

---
+++ Security - ex02/src/main/java/com/cos/blog/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,30 @@
+package com.cos.blog.config.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cos.blog.domain.user.User;
+import com.cos.blog.domain.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PrincipalDetailsService implements UserDetailsService{
+
+	private final UserRepository userRepository; 
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// /login 이 호출되면 자동 실행되어 username이 DB에 있는지 확인한다. 
+		User principal = userRepository.findByUsername(username);
+		
+		if(principal == null) {
+			return null;
+		}else {
+			return new PrincipalDetails(principal);   // 비밀번호 검증을 알아서한다 .
+		}
+	}
+}

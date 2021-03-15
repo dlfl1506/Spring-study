5c39feb54a19027a289606df3794af7efec9be7b
YD <hoggaz@naver.com>
2021-03-15 13:11:28 +0900

    5-6. oAuth 로그인 (카카오 로그인)

---
+++ Security - ex07 kakao/src/main/java/com/cos/blog/domain/post/Post.java
@@ -0,0 +1,51 @@
+package com.cos.blog.domain.post;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.cos.blog.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Post {
+	
+	@Id // Primary key 
+	@GeneratedValue(strategy =GenerationType.IDENTITY )  // Table,Auto_increment,Sequence
+	private Integer id;
+	
+	@Column(nullable =false ,length = 100)
+	private String title;
+	
+	@Lob // 대용량 데이터
+	private String content;
+	
+	@ColumnDefault("0")  // null 일시 자동으로 디폴트값 0 이 이들어감
+	private int count;
+	
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User user;
+	
+	@CreationTimestamp 
+	private Timestamp createDate;
+
+}
